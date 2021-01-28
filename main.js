e06db2fcace29f6b224a53941bbb907850371353
captain guliye <guliye91@gmail.com>
2021-01-28 16:54:23 +0300

    added another js tricky

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const tabItems = document.querySelectorAll('.tab-item');
 const tabContent = document.querySelectorAll('.tab-content-item');
+const tabOneContents = document.querySelector('.tab-1-contents')
+const tabTwoContents = document.querySelector('.tab-2-contents')
+const tabThreeContents = document.querySelector('.tab-3-contents')
 
 // select tab content item
 function selectItem(e){
@@ -8,9 +11,17 @@ function selectItem(e){
     //add border to current tab
     this.classList.add('tab-border');
     // grab content item for dom
-    const tabContentItem = document.querySelector(`#${this.id}-content`);
-    console.log(this.id)
-    tabContentItem.classList.add('show');
+    // const tabContentItem = document.querySelector(`#${this.id}-content`);
+    // console.log(this.id)
+    // tabContentItem.classList.add('show');
+    if(this.classList.contains('tab-1')){
+        tabOneContents.classList.add('show');
+    }else if(this.classList.contains('tab-2')){
+        tabTwoContents.classList.add('show');
+    }
+    else{
+        tabThreeContents.classList.add('show');
+    }
 }
 
 function removeBorder() {
